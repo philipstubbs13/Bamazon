88cd6743a2a3430ecf3d3f1c01343c4c72b1c30a
Phil S <philipstubbs13@gmail.com>
2018-02-16 19:08:47 -0600

    added view low inventory functionality to manager view

--- bamazonManager.js
+++ bamazonManager.js
@@ -44,7 +44,7 @@ function showStartScreen() {
 		}
 
 		else if (answers.managerList === "View Low Inventory") {
-			console.log("view low inventory");
+			viewLowInventory();
 		}
 
 		else if (answers.managerList === "Add to Inventory") {
@@ -70,10 +70,29 @@ function showProductsForSale() {
 			"Price: $" + res[i].price + "\r\n" +
 			"Department: " + res[i].department_name + "\r\n" +
 			"Quantity in stock: " + res[i].stock_quantity + "\r\n" +
-			("=====================================")
+			"====================================="
 			console.log(items);
 		}
-	connection.end();
+		connection.end();
 	});
+}
+
+function viewLowInventory() {
+	connection.query("SELECT item_id, product_name, stock_quantity FROM products WHERE stock_quantity < 5", function(err,res){
+		if (err) throw err;
+
+		console.log("The following products are low in inventory (quantity is less than 5.)");
+		//console.log(res);
+		for (var i = 0; i < res.length; i++){
+			var lowInvetoryItems =
+			"====================================" + "\r\n" +
+			"Item number: " + res[i].item_id + "\r\n" +
+			"Item: " + res[i].product_name + "\r\n" +
+			"Quantity in stock: " + res[i].stock_quantity + "\r\n" +
+			"====================================" 
+			console.log(lowInvetoryItems);
+		}
+		connection.end();
+	})
 }
 showStartScreen();
